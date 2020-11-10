3407d6e8dc943796b9b94abec9f730dfe21c0971
Federico Soncini <federicosoncini@Federicos-Air.fritz.box>
2020-11-10 16:18:20 +0100

    ex 3-4

--- script.js
+++ script.js
@@ -8,20 +8,118 @@ window.onload = async () => {
 
   for (let i = 0; i < users_parsed.length; i++) {
     inner = `<div class="col col-6">
-    <div class="card" style="width: 18rem;">
-        <div class="card-header">
-            Featured
-        </div>
-        <ul class="list-group list-group-flush">
-            <li class="list-group-item">${users_parsed[i].name}</li>
-            <li class="list-group-item">${users_parsed[i].username} in</li>
-            <li class="list-group-item">${users_parsed[i].email}</li>
-        </ul>
-    </div>
-</div>`;
+      <div class="card" style="width: 18rem;">
+          <div class="card-header">
+              Featured
+          </div>
+          <ul class="list-group list-group-flush">
+              <li class="list-group-item">${users_parsed[i].name}</li>
+              <li class="list-group-item">${users_parsed[i].username} in</li>
+              <li class="list-group-item">${users_parsed[i].email}</li>
+          </ul>
+      </div>
+  </div>`;
 
     row.innerHTML += inner;
   }
 
+  let name = document.querySelector("option:nth-of-type(1)").value;
+  let username = document.querySelector("option:nth-of-type(2)").value;
+  let email = document.querySelector("option:nth-of-type(3)").value;
+  let filter = document.getElementById("filter").value;
+  let searchInput = document.querySelector("input:nth-of-type(1)").value;
+  let submitBtn = document.getElementById("submit");
+
+  const filterByUserName = () => {
+    let filtered = users_parsed.filter((found) =>
+      found.username.includes(searchInput)
+    );
+    console.log(filtered);
+    row.innerHTML = "";
+    let cardFilered = `<div class="col col-6">
+      <div class="card" style="width: 18rem;">
+          <div class="card-header">
+              Featured
+          </div>
+          <ul class="list-group list-group-flush">
+              <li class="list-group-item">${filtered[0].name}</li>
+              <li class="list-group-item">${filtered[0].username} in</li>
+              <li class="list-group-item">${filtered[0].email}</li>
+          </ul>
+      </div>
+      </div>`;
+    row.innerHTML = row.innerHTML + cardFilered;
+    filtered = [];
+  };
+
+  const filterByEmail = () => {
+    let filtered = users_parsed.filter((found) =>
+      found.email.includes(searchInput)
+    );
+    console.log(filtered);
+    row.innerHTML = "";
+    let cardFilered = `<div class="col col-6">
+      <div class="card" style="width: 18rem;">
+          <div class="card-header">
+              Featured
+          </div>
+          <ul class="list-group list-group-flush">
+              <li class="list-group-item">${filtered[0].name}</li>
+              <li class="list-group-item">${filtered[0].username} in</li>
+              <li class="list-group-item">${filtered[0].email}</li>
+          </ul>
+      </div>
+      </div>`;
+    row.innerHTML = row.innerHTML + cardFilered;
+    filtered = [];
+  };
+
+  const filterByName = () => {
+    let filtered = users_parsed.filter((found) =>
+      found.name.includes(searchInput)
+    );
+    console.log(filtered);
+    row.innerHTML = "";
+    let cardFilered = `<div class="col col-6">
+      <div class="card" style="width: 18rem;">
+          <div class="card-header">
+              Featured
+          </div>
+          <ul class="list-group list-group-flush">
+              <li class="list-group-item">${filtered[0].name}</li>
+              <li class="list-group-item">${filtered[0].username}</li>
+              <li class="list-group-item">${filtered[0].email}</li>
+          </ul>
+      </div>
+      </div>`;
+    row.innerHTML = row.innerHTML + cardFilered;
+    filtered = [];
+  };
+
+  if (filter === name) {
+    submitBtn.addEventListener("click", filterByName);
+  } else if (filter === username) {
+    submitBtn.addEventListener("click", filterByUserName);
+  } else if (filter === email) {
+    submitBtn.addEventListener("click", filterByEmail);
+  }
   console.log(users_parsed);
+
+  const extractName = () => {
+    let name = users_parsed.map((n) => n.name);
+    return name;
+  };
+
+  console.log(extractName());
+
+  const createAddress = () => {
+    // const { city, street, suite, zip_code } = users_parsed[0].address;
+
+    users_parsed.forEach((user) => {
+      const { city, street, suite, zipcode } = user.address;
+      let personal_address = city + " " + street + " " + suite + " " + zipcode;
+      console.log(personal_address);
+    });
+  };
+  createAddress();
 };
